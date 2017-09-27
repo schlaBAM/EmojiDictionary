3d018fcf1e11a631782a71bd793f51136fe990f0
schlaBAM <brett.morrison@live.com>
2017-09-26 17:55:04 -0700

    both VCs started, 33% tableview implementation complete

--- EmojiDictionary/ViewController.swift
+++ EmojiDictionary/ViewController.swift
@@ -8,16 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var tableView: UITableView!
+    let emojiArray: [String] = ["🍆","🍑","👉🏿","✊","👌","🖕","🐹","🙈","🍕","💯","🅱️","🇨🇦"]
+    
+    override func viewDidLoad(){
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojiArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojiArray[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let cell = tableView.cellForRow(at: indexPath)
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 
